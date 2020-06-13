60715be28a99066ce1819d0065f06cffb4af2a48
malikkubs <malikkubs@gmail.com>
2020-06-13 22:21:05 +0700

    ngobar bersama sandika eps17 di laravel

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    $nama = 'malik';
+    return view('about', ['nama' => $nama]);
 });
