c53b0ecdba723a64795ee4979e644b3d772dd2de
malikkubs <malikkubs@gmail.com>
2020-06-13 22:22:28 +0700

    ngobar 17 bersama sandika galih

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout/main')
+@section('title', 'web malik aja')
+@section('container')
+<div class="container">
+  <div class="row">
+    <div class="col-10">
+      <h1 class="mt-3">Hello, world!</h1>
+    </div>
+  </div>
+</div>
+@endsection
